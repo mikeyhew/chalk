873db9985518c4cb2ff8edb30b902500a3514a57
Niko Matsakis <niko@alum.mit.edu>
2017-10-04 05:36:25 -0500

    improve REPL with command-line options and in particular --slg

--- src/bin/chalki.rs
+++ src/bin/chalki.rs
@@ -1,6 +1,10 @@
 extern crate rustyline;
 extern crate chalk_parse;
 extern crate chalk;
+extern crate docopt;
+
+#[macro_use]
+extern crate serde_derive;
 
 #[macro_use]
 extern crate error_chain;
@@ -8,12 +12,37 @@ extern crate error_chain;
 use std::io::Read;
 use std::fs::File;
 use std::sync::Arc;
+use std::process::exit;
 
 use chalk::ir;
 use chalk::lower::*;
 use chalk::solve::solver::{self, Solver, CycleStrategy};
-
+use chalk::solve::slg;
+use docopt::Docopt;
 use rustyline::error::ReadlineError;
+
+const USAGE: &'static str = "
+chalk repl
+
+Usage:
+  chalki [options]
+  chalki (-h | --help)
+
+Options:
+  --help              Show this screen.
+  --program=PATH      Specifies the path to the `.chalk` file containing traits/impls.
+  --goal=GOAL         Specifies a goal to evaluate (may be given more than once).
+  --overflow-depth=N  Specifies the overflow depth (default: 10).
+  --slg               Use the experimental SLG resolution system.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    flag_program: Option<String>,
+    flag_goal: Vec<String>,
+    flag_overflow_depth: usize,
+    flag_slg: bool,
+}
 
 error_chain! {
     links {
@@ -44,16 +73,54 @@ impl Program {
 quick_main!(run);
 
 fn run() -> Result<()> {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+
     // Initialize global overflow depth before everything
-    let overflow_depth = 10;
-    solver::set_overflow_depth(overflow_depth);
+    solver::set_overflow_depth(args.flag_overflow_depth);
 
     let mut prog = None;
-    readline_loop(&mut rustyline::Editor::new(), "?- ", |rl, line| {
-        if let Err(e) = process(line, rl, &mut prog) {
-            println!("error: {}", e);
+
+    if let Some(program) = args.flag_program {
+        match load_program(&program) {
+            Ok(p) => prog = Some(p),
+            Err(err) => {
+                println!("error loading program: {}", err);
+                exit(1);
+            }
         }
-    })
+    }
+
+    let use_slg = args.flag_slg;
+    if args.flag_goal.is_empty() {
+        readline_loop(&mut rustyline::Editor::new(), "?- ", |rl, line| {
+            if let Err(e) = process(use_slg, line, rl, &mut prog) {
+                println!("error: {}", e);
+            }
+        })
+    } else {
+        let prog = match prog {
+            Some(p) => p,
+            None => {
+                println!("error: cannot eval with a program, use `--program`");
+                exit(1);
+            }
+        };
+
+        let flag_goal = args.flag_goal;
+        ir::set_current_program(&prog.ir, || -> Result<()> {
+            for g in flag_goal {
+                if let Err(e) = goal(use_slg, &g, &prog) {
+                    println!("error: {}", e);
+                    exit(1);
+                }
+            }
+            Ok(())
+        })?;
+
+        Ok(())
+    }
 }
 
 /// Repeatedly calls `f`, passing in each line, using the given promt, until EOF is received
@@ -75,29 +142,37 @@ fn readline_loop<F>(rl: &mut rustyline::Editor<()>, prompt: &str, mut f: F) -> R
 }
 
 /// Process a single command
-fn process(command: &str, rl: &mut rustyline::Editor<()>, prog: &mut Option<Program>) -> Result<()> {
+fn process(use_slg: bool,
+           command: &str,
+           rl: &mut rustyline::Editor<()>,
+           prog: &mut Option<Program>)
+           -> Result<()> {
     if command == "help" {
         help()
     } else if command == "program" {
         *prog = Some(Program::new(read_program(rl)?)?);
     } else if command.starts_with("load ") {
         let filename = &command["load ".len()..];
-        let mut text = String::new();
-        File::open(filename)?.read_to_string(&mut text)?;
-        *prog = Some(Program::new(text)?);
+        *prog = Some(load_program(filename)?);
     } else {
         let prog = prog.as_ref().ok_or("no program currently loaded")?;
         ir::set_current_program(&prog.ir, || -> Result<()> {
             match command {
                 "print" => println!("{}", prog.text),
                 "lowered" => println!("{:#?}", prog.env),
-                _ => goal(command, prog)?,
+                _ => goal(use_slg, command, prog)?,
             }
             Ok(())
         })?
     }
 
     Ok(())
+}
+
+fn load_program(filename: &str) -> Result<Program> {
+    let mut text = String::new();
+    File::open(filename)?.read_to_string(&mut text)?;
+    Ok(Program::new(text)?)
 }
 
 fn help() {
@@ -120,13 +195,24 @@ fn read_program(rl: &mut rustyline::Editor<()>) -> Result<String> {
     Ok(text)
 }
 
-fn goal(text: &str, prog: &Program) -> Result<()> {
+fn goal(use_slg: bool, text: &str, prog: &Program) -> Result<()> {
     let goal = chalk_parse::parse_goal(text)?.lower(&*prog.ir)?;
-    let mut solver = Solver::new(&prog.env, CycleStrategy::Tabling, solver::get_overflow_depth());
-    let goal = ir::InEnvironment::new(&ir::Environment::new(), *goal);
-    match solver.solve_closed_goal(goal) {
-        Ok(v) => println!("{}\n", v),
-        Err(e) => println!("No possible solution: {}\n", e),
+    if use_slg {
+        match slg::solve_repl(solver::get_overflow_depth(), &prog.env, *goal) {
+            Ok(answers) => {
+                println!("{} answers found: {:#?}\n", answers.answers.len(), answers);
+            }
+            Err(error) => {
+                println!("exploration error: {:?}\n", error);
+            }
+        }
+    } else {
+        let mut solver = Solver::new(&prog.env, CycleStrategy::Tabling, solver::get_overflow_depth());
+        let goal = ir::InEnvironment::new(&ir::Environment::new(), *goal);
+        match solver.solve_closed_goal(goal) {
+            Ok(v) => println!("{}\n", v),
+            Err(e) => println!("No possible solution: {}\n", e),
+        }
     }
     Ok(())
 }
